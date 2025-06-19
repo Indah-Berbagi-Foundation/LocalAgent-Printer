9430523e147a98eebb72de368cdabbc884673e2b
muhismail015 <muhismail7@gmail.com>
2025-06-19 17:19:49 +0700

    refactor + support base64

--- index.js
+++ index.js
@@ -1,37 +1,62 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import http from "http";
+
 import { Printer, Image } from "@node-escpos/core";
 import USB from "@node-escpos/usb-adapter";
 import sharp from "sharp";
-import path from "path";
-import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const imagePath = path.join(__dirname, "assets/img/invoice-1749541455175.png");
+const PORT = process.env.PORT;
 
-const resizedImagePath = path.join(
-  __dirname,
-  "assets/temp/resized-invoice.png"
-);
-const resizeImage = async () => {
-  await sharp(imagePath).resize({ width: 384 }).toFile(resizedImagePath);
-};
+try {
+  const app = express();
 
-const device = new USB();
+  app.use(express.json({ limit: "10mb" }));
 
-device.open(async (err) => {
-  if (err) {
-    console.error("Error opening device:", err);
-    return;
-  }
+  app.post("/print", async (req, res) => {
+    const { image } = req.body;
 
-  try {
-    await resizeImage();
+    const base64Data = image.replace(/^data:image\/\w+;base64,/, "");
+    const buffer = Buffer.from(base64Data, "base64");
 
-    const image = await Image.load(resizedImagePath);
-    const printer = new Printer(device, {});
+    try {
+      const resizedBuffer = await sharp(buffer)
+        .resize({ width: 384 })
+        .png()
+        .toBuffer();
 
-    printer.align("ct").raster(image).cut().close();
-  } catch (e) {
-    console.error("Print error:", e);
-  }
-});
+      const device = new USB();
+      device.open(async (err) => {
+        if (err) {
+          console.error("Error opening device:", err);
+          return res.status(500).json({ message: "Failed to open printer" });
+        }
+
+        const escposImage = await Image.load(resizedBuffer, "image/png");
+        const printer = new Printer(device, {});
+        printer.align("ct").raster(escposImage).cut().close();
+      });
+
+      console.log("Image sent to printer successfully");
+      return res
+        .status(200)
+        .json({ message: "Success, Image sent to printer" });
+    } catch (err) {
+      console.error("Print error:", err);
+      return res
+        .status(500)
+        .json({ message: "Failed to print image", error: err.message });
+    }
+  });
+
+  const server = http.createServer(app);
+
+  server.listen(PORT, () => {
+    console.log(`Local printer agent running on port ${PORT}`);
+  });
+} catch (error) {
+  console.error("Error starting server:", error);
+  process.exit(1);
+}
